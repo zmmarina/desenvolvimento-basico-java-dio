ad12e3798de491b36eb44aef74b2a6ed44051b47
Zandra Monteiro <zandra.monteiro@liferay.com>
2021-06-22 14:40:43 -0300

    class created

--- src/com/zmonteiro/exercise01/Car.java
+++ src/com/zmonteiro/exercise01/Car.java
@@ -1,5 +1,47 @@
 package com.zmonteiro.exercise01;
 
 public class Car {
+    public String brand;
+    public String model;
+    public Integer year;
+    public String variant;
 
+    public Car(String brand, String model, Integer year, String variant) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.variant = variant;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getVariant() {
+        return variant;
+    }
+
+    public void setVariant(String variant) {
+        this.variant = variant;
+    }
 }
